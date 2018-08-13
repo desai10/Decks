7479b912d7697506cbab2f6eb6ba8936c7dcfc7e
srdesai <isriramdesai@gmail.com>
2018-08-13 11:47:16 +0530

    Init.

---
+++ src/Utils.java
@@ -0,0 +1,8 @@
+public class Utils {
+
+    public enum Suits {Clubs, Hearts, Diamonds, Spade};
+
+    public enum pip {Joker, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Jack, Queen, King, Ace};
+
+
+}
