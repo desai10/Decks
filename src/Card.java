745f4582874378b084e0b55292685039d7ab54f8
srdesai <isriramdesai@gmail.com>
2018-08-13 12:04:27 +0530

    Added card class.

--- src/Card.java
+++ src/Card.java
@@ -1,2 +1,23 @@
 public class Card {
+
+    Utils.Suits suit;
+    Utils.Pip pip;
+    boolean isWildCard;
+
+    public Card(String suit, String pip) {
+        this.suit = Utils.Suits.valueOf(suit);
+        this.pip = Utils.Pip.valueOf(pip);
+        isWildCard = false;
+    }
+
+    public Card(Utils.Suits suit, Utils.Pip pip) {
+        this.suit = suit;
+        this.pip = pip;
+        isWildCard = false;
+    }
+
+    public void setWildCard(boolean wildCard) {
+        isWildCard = wildCard;
+    }
+
 }
