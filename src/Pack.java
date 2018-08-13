48fe79c6b74f00b824b91a8e79fd509262dff22e
aishwaryavasu <aishwaryavasu1997@gmail.com>
2018-08-13 12:22:36 +0530

    Added pack and comparators

--- src/Pack.java
+++ src/Pack.java
@@ -6,10 +6,14 @@ public class Pack {
 
     public static List<Card> getPackCards() {
         List<Card> cards = new ArrayList<>();
-        for(Utils.Suits suit : Utils.Suits.values())
-            for(Utils.Pip pip : Utils.Pip.values())
-                cards.add(new Card(suit,pip));
-    return cards;
+        Utils.Suits suitArray[] = Utils.Suits.values();
+        Utils.Pip pipArray[]=Utils.Pip.values();
+        for(int i=1;i<suitArray.length;i++)
+            for(int j=1;j<pipArray.length;i++)
+                cards.add(new Card(suitArray[i],pipArray[j]));
+        cards.add(new Card(suitArray[0],pipArray[0]));
+        cards.add(new Card(suitArray[0],pipArray[0]));
+        return cards;
     }
 
 
