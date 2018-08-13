6ec8f3a7120c019ba0b58152f2a62d72001b0965
Adarsh Srivastava <adarsh0112@gmail.com>
2018-08-13 14:15:31 +0530

    modified deal, removed Pack class

--- src/Deck.java
+++ src/Deck.java
@@ -5,13 +5,13 @@ public class Deck {
 
     Deck(int numberOfPacks){
         while(numberOfPacks--!=0)
-            allCards.addAll(Pack.getPackCards());
+            allCards.addAll(getPack());
     }
 
     Deck(int numberOfPacks, int numberOfjokers){
         int numberOfJokersToRemove=2*numberOfPacks-numberOfjokers;
         while(numberOfPacks--!=0)
-            allCards.addAll(Pack.getPackCards());
+            allCards.addAll(getPack());
         while(numberOfJokersToRemove--!=0)
             removeJokerFromDeck();
     }
@@ -31,18 +31,39 @@ public class Deck {
     }
 
     public List<List<Card>> deal(int numberOfPlayers){
-        //this.shuffle();
+        int cardsPerPlayer=allCards.size()/numberOfPlayers;
+        return deal(numberOfPlayers, cardsPerPlayer);
+    }
+
+    public List<List<Card>> deal(int numberOfPlayers, int cardsPerPlayer){
+        if(cardsPerPlayer*numberOfPlayers>allCards.size())
+            return NULL;
+        this.shuffle();
         List<List<Card>> gameSet=new ArrayList<List<Card>>();
+        Iterator<Card> iter=allCards.iterator();
         for(int i=0; i<numberOfPlayers; i++)
             gameSet.add(new ArrayList<Card>());
-        int player=0;
-        Iterator<Card> iter=allCards.iterator();
-        while(iter.hasNext()){
-            gameSet.get(player).add(iter.next());
-            player=(player+1)%numberOfPlayers;
+        for(int i=0; i<numberOfPlayers && iter.hasNext(); i++) {
+            int cardsdealt=0;
+            while(cardsdealt<cardsPerPlayer) {
+                gameSet.get(i).add(iter.next());
+                cardsdealt++;
+            }
         }
         System.out.println(gameSet);
         return gameSet;
+    }
+
+    public static List<Card> getPack() {
+        List<Card> cards = new ArrayList<>();
+        Utils.Suits suitArray[] = Utils.Suits.values();
+        Utils.Pip pipArray[]=Utils.Pip.values();
+        for(int i=1;i<suitArray.length;i++)
+            for(int j=1;j<pipArray.length;j++)
+                cards.add(new Card(suitArray[i],pipArray[j]));
+        cards.add(new Card(suitArray[0],pipArray[0]));
+        cards.add(new Card(suitArray[0],pipArray[0]));
+        return cards;
     }
 
 }
